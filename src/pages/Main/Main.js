338daa07910696a96913c7eaad9862c27b33b6c5
정인호 <wjddlsgh0203@gmail.com>
2024-10-01 19:28:49 +0900

    fetch 에러 해결중4

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -36,7 +36,7 @@ const Main = () => {
 
   useEffect(() => {
     // fetch('https://port-0-zinwoos-backend-fork-m1kb43jnab9bc7ab.sel4.cloudtype.app/', {
-    fetch('http://1.245.156.117:3000/', {
+    fetch('https://1.245.156.117:3000/', {
       method: 'GET',
       credentials: 'include'
     })
@@ -59,7 +59,7 @@ const Main = () => {
 
   useEffect(() => {
     // fetch('https://port-0-zinwoos-backend-fork-m1kb43jnab9bc7ab.sel4.cloudtype.app/items/new')
-    fetch('http://1.245.156.117:3000/items/new')
+    fetch('https://1.245.156.117:3000/items/new')
       .then(response => response.json())
       .then(result => {
         setNewList(result.data);
