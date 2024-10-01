ecf19d85e86767b98ab99b81a250458a3ce22963
정인호 <wjddlsgh0203@gmail.com>
2024-10-01 19:23:46 +0900

    fetch 에러 해결중3

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -35,7 +35,8 @@ const Main = () => {
   const [newList, setNewList] = useState([]);
 
   useEffect(() => {
-    fetch('https://port-0-zinwoos-backend-fork-m1kb43jnab9bc7ab.sel4.cloudtype.app/', {
+    // fetch('https://port-0-zinwoos-backend-fork-m1kb43jnab9bc7ab.sel4.cloudtype.app/', {
+    fetch('http://1.245.156.117:3000/', {
       method: 'GET',
       credentials: 'include'
     })
@@ -57,7 +58,8 @@ const Main = () => {
   }, [])
 
   useEffect(() => {
-    fetch('https://port-0-zinwoos-backend-fork-m1kb43jnab9bc7ab.sel4.cloudtype.app/items/new')
+    // fetch('https://port-0-zinwoos-backend-fork-m1kb43jnab9bc7ab.sel4.cloudtype.app/items/new')
+    fetch('http://1.245.156.117:3000/items/new')
       .then(response => response.json())
       .then(result => {
         setNewList(result.data);
